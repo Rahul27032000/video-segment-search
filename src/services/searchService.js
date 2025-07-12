d93a79ec87178285d736732becfe108d3ba954d5
Rahul2732000 <heyrahul327@gmail.com>
2025-07-12 11:49:41 +0530

    create end to end working genai project

--- src/services/searchService.js
+++ src/services/searchService.js
@@ -2,18 +2,21 @@ import pool from "../config/dbConfig.js";
 import { getTextEmbedding } from "./embedService.js";
 
 export async function searchBestChunk(queryText) {
-  const embedding = await getTextEmbedding(queryText);
-  // const embedding = "[0.123, 0.456, 0.789]"
+  const embeddingArr = await getTextEmbedding(queryText);
+  const embeddingVec = `[${embeddingArr.join(",")}]`; 
   const sql = `
-    SELECT vcm."videoId", c."startTime", c."endTime", c.text,
-             1 - (c."embedding" <#> $1) AS score
-      FROM "Chunk" c
-      JOIN "VideoChunkMap" vcm ON c.id = vcm."chunkId"
-      WHERE vcm."isActive" = true
-      ORDER BY c."embedding" <#> $1
-      LIMIT 1;
+    SELECT vcm."videoId",
+           c."startTime",
+           c."endTime",
+           c.text,
+           1 - (c."embedding" <#> $1::vector) AS score    -- 👈 cast placeholder
+    FROM "Chunk" c
+    JOIN "VideoChunkMap" vcm ON c.id = vcm."chunkId"
+    WHERE vcm."isActive" = true
+    ORDER BY c."embedding" <#> $1::vector
+    LIMIT 1;
   `;
-  const { rows } = await pool.query(sql, [embedding]);
-  console.log(rows);
+
+  const { rows } = await pool.query(sql, [embeddingVec]); // 👈 pass string
   return rows[0];
 }
